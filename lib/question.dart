2be482e7b92e01ae0c4f63610e67cd22fcb005f8
Muhammad Mazhar <mazhar6542@gmail.com>
2022-04-06 23:30:32 +0500

    Revert "Revert "Quiz App-Assignment number 07""
    This reverts commit aa94ae4c5c35de811ed495dbb6e1404c1f8e2cd9.

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String questionText;
+  List<String> data;
+  int index;
+
+  Question(String q, List<String> data_,int a) {
+    questionText = q;
+    data=data_;
+    index = a;
+  }
+}
