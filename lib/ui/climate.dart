b744f37fa50b53246604d3ea8a0a938e50d6de95
Muhammad Mazhar <mazhar6542@gmail.com>
2022-04-18 23:36:58 +0500

    Starting The Weather App

---
+++ lib/ui/climate.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class climate extends StatefulWidget {
+
+
+  @override
+  State<climate> createState() => _climateState();
+}
+
+class _climateState extends State<climate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
