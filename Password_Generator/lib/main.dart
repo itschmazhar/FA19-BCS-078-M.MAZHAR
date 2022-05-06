c295fcdbd87dcbc94f1ecfb7202e9b9406a6058e
Muhammad Mazhar <mazhar6542@gmail.com>
2022-05-06 21:50:51 +0500

    Changes in main file (Add Background Gradient Colour)

--- Password_Generator/lib/main.dart
+++ Password_Generator/lib/main.dart
@@ -139,7 +139,14 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                //  colors: [Colors.pink, Colors.red, Colors.orange],
+                colors: [
+              Colors.greenAccent,
+              Colors.blue,
+              Colors.blueAccent,
+            ], begin: Alignment.bottomLeft, end: Alignment.topRight)),
       ),
     );
   }
